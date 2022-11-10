88ab267671ef35f9be10febbda0b526eea909009
陈阳 <775517776@qq.com>
2022-11-10 15:46:21 +0800

    “更换openGauss书记员仓库”

--- src/main/resources/script/opengauss/initDoc.sh
+++ src/main/resources/script/opengauss/initDoc.sh
@@ -12,7 +12,7 @@ mkdir -p /usr/local/docs/target/en/
 
 # shellcheck disable=SC2164
 cd /usr/local/docs/source
-git clone https://gitee.com/ailoooong/gaussv2.git
+git clone https://gitee.com/opengauss/website.git
 # shellcheck disable=SC2164
 cd ./gaussv2
 #pnpm install
