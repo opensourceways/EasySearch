3cc861ee2b4678b89d2cf20c22ecc3dec6bdae77
陈阳 <775517776@qq.com>
2023-05-22 15:13:29 +0800

    1.index锁方法优化

--- src/main/java/com/search/docsearch/service/DataImportService.java
+++ src/main/java/com/search/docsearch/service/DataImportService.java
@@ -5,11 +5,15 @@ import java.util.Set;
 
 public interface DataImportService {
 
-    void refreshDoc() throws IOException;
+    void refreshDoc();
 
     void sendKafka(String data, String parameter);
 
     void listenKafka();
 
     void deleteExpired(Set<String> idSet);
+
+    void globalLock();
+
+    void globalUnlock();
 }
