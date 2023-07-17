a9554dce4bef083ac6b36a82fe0720ea7de4ac9d
陈阳 <775517776@qq.com>
2023-07-17 19:40:30 +0800

    删除配置文件流程修改，完整写法

--- src/main/java/com/search/docsearch/constant/Constants.java
+++ src/main/java/com/search/docsearch/constant/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final long MILLISECONDS_OF_A_DAY = 86400000;
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String SHANGHAI_TIME_ZONE = "Asia/Shanghai";
+    public static final String CONFIG_PATH = "/EaseSearch/target/classes/application.yml";
 }
 
 
