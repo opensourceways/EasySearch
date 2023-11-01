55010071c03cc57145e5e667053f59e4e49c41a8
陈阳 <775517776@qq.com>
2023-11-01 15:11:00 +0800

    参数校验更新

--- src/main/java/com/search/docsearch/entity/vo/SearchCondition.java
+++ src/main/java/com/search/docsearch/entity/vo/SearchCondition.java
@@ -35,7 +35,7 @@ public class SearchCondition {
     @Size(max = 20)
     private String keyword;
     
-    @Pattern(regexp = "((^migration$|^news$|^forum$|^blog$|^docs$|^showcase$|^other$|^service$))", message = "Keyword format is invalid")
+    @Pattern(regexp = "((^showcase$|^service$|^other$|^news$|^migration$|^forum$|^docs$|^blog$|^events$|^blogs$|^tutorials$|^install$|^information$|^api$|^video$|^activity$)|^$)", message = "Keyword format is invalid")
     private String type;
 
     private List<Map<String, String>> limit;
