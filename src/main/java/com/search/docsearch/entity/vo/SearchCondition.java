982c4d48a3e41abc76ba95a3dd1b0a320222aa99
陈阳 <775517776@qq.com>
2023-11-10 10:54:17 +0800

    mindspore搜索分页需求为8~48页，扩大分页限制

--- src/main/java/com/search/docsearch/entity/vo/SearchCondition.java
+++ src/main/java/com/search/docsearch/entity/vo/SearchCondition.java
@@ -25,7 +25,7 @@ public class SearchCondition {
     @Range(min = 1, max = 1000, message = "page must be greater than 0 and less than 1000 ")
     private int page = 1;
 
-    @Range(min = 5, max = 20, message = "page must be greater than 5 and less than 20 ")
+    @Range(min = 5, max = 50, message = "page must be greater than 5 and less than 50 ")
     private int pageSize = 10;
 
     @NotBlank(message = "keyword can not be null")
