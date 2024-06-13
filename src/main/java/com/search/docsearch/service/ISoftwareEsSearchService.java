923a9dfc6354181a0fa9e945d110e95c164dbf78
zwjsec <zhaiwenjiesec@163.com>
2024-06-13 16:41:15 +0800

    bug fixed

--- src/main/java/com/search/docsearch/service/ISoftwareEsSearchService.java
+++ src/main/java/com/search/docsearch/service/ISoftwareEsSearchService.java
@@ -1,7 +1,5 @@
 package com.search.docsearch.service;
 
-
-
 import com.search.docsearch.dto.software.SearchFindwordDto;
 import com.search.docsearch.dto.software.SearchTagsDto;
 import com.search.docsearch.entity.software.*;
@@ -10,17 +8,13 @@ import com.search.docsearch.except.ServiceException;
 import java.util.List;
 import java.util.Map;
 
-
 public interface ISoftwareEsSearchService {
 
     SoftwareSearchResponce searchByCondition(SoftwareSearchCondition condition) throws ServiceException;
 
     List<SearchTagsDto> getTags(SoftwareSearchTags searchTags) throws ServiceException;
 
-    SearchFindwordDto findWord(String prefix, String dataType) throws ServiceException;
-
-
     List<SoftwareSearchCountResponce> getCountByCondition(SoftwareSearchCondition condition) throws ServiceException;
 
-    List<SoftwareDocsAllResponce>  searchAllByCondition(SoftwareSearchCondition condition) throws ServiceException;
+    List<SoftwareDocsAllResponce> searchAllByCondition(SoftwareSearchCondition condition) throws ServiceException;
 }
