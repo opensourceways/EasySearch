152307070f27ca4f3e7c8f2cc0be1ca87bba32a2
leefan <980488905@qq.com>
2024-08-30 17:24:35 +0800

    bug 修复

--- src/main/java/com/search/docsearch/entity/software/SoftwareSearchCondition.java
+++ src/main/java/com/search/docsearch/entity/software/SoftwareSearchCondition.java
@@ -31,16 +31,16 @@ public class SoftwareSearchCondition implements Cloneable {
     @Size(max = 10)
     @Pattern(regexp = "((^oepkg$|^all$|^rpmpkg$|^apppkg$|^epkgpkg$|^appversion$|^$))")
     private String dataType;
-    @Size(max = 30)
+    @Size(max = 300)
     @Pattern(regexp = "^$|^[\\u4E00-\\u9FA5A-Za-z0-9.()$\\-_+, ]+$", message = "Null or string. String includes only letters, digits, and special ")
     private String version;
-    @Size(max = 100)
+    @Size(max = 300)
     @Pattern(regexp = "^$|^[\\u4E00-\\u9FA5A-Za-z0-9.()$\\-_+, ]+$", message = "Null or string. String includes only letters, digits, and special ")
     private String os;
-    @Size(max = 30)
+    @Size(max = 300)
     @Pattern(regexp = "^$|^[\\u4E00-\\u9FA5A-Za-z0-9.()$\\-_+, ]+$", message = "Null or string. String includes only letters, digits, and special ")
     private String arch;
-    @Size(max = 30)
+    @Size(max = 300)
     @Pattern(regexp = "^$|^[\\u4E00-\\u9FA5A-Za-z0-9.()$\\-_+, ]+$", message = "Null or string. String includes only letters, digits, and special ")
     private String category;
     @Size(max = 30)
@@ -49,7 +49,7 @@ public class SoftwareSearchCondition implements Cloneable {
     @Size(max = 5)
     @Pattern(regexp = "((^desc$|^asc$|^$))")
     private String nameOrder;
-    @Size(max = 30)
+    @Size(max = 300)
     @Pattern(regexp = "^$|^[\\u4E00-\\u9FA5A-Za-z0-9.()$\\-_+, ]+$", message = "Null or string. String includes only letters, digits, and special ")
     private String eulerOsVersion;
     @Size(max = 5)
